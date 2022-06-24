ab113c92cb26347c61f92ed3a85749596e4c2b71
Leonardo Alves <leonardo.alves@eritel.com.br>
2022-06-24 13:11:27 -0300

    Add project files.

---
+++ Linked List Cycle II/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace Linked_List_Cycle_II
+{
+    public class Solution
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+        public ListNode DetectCycle(ListNode head)
+        {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while (fast != null && fast.next !=null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow)
+                {
+                    slow = head;
+                    while (slow != fast)
+                    {
+                        fast = fast.next;
+                        slow = slow.next;
+                    }
+                    return slow;
+                }
+            }
+            return null;
+        }
+    }
+}
